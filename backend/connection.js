6b0732bdfaab323737b774ffe7b2a8eb36cb4d43
panshuman280 <p.anshuman098@gmail.com>
2022-12-18 19:50:23 +0530

    url

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbName = "wss2000";
-const url = `mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const url = `mongodb+srv://panshuman098:Satyam123@cluster0.qlch9jg.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 // Asynchronous Fuction - returns promise
 mongoose.connect(url)
